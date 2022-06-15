f514950291e16379915712c9ec5202c197522a37
Thiago Sullivan <thiagosullivan12@gmail.com>
2022-06-15 11:49:15 -0300

    styled theme and favicon

--- styles/theme.js
+++ styles/theme.js
@@ -5,8 +5,8 @@ const theme = {
     fourth: '#B4E128',
     gradient: 'linear-gradient(225deg, #B4E128 0%, #A3D212 100%);',
     grey: '#F5F5F5',
-    greysecond: '#E5E5E5',
-    greythird: '#909090',
+    greysecond: '#909090',
+    greythird: '#333333',
     white: '#FFFFFF',
     error: '#D45D5D',
     success: '#6fd45d'
