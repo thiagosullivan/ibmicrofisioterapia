875efb3bcee6c6244dfc90a09c6824e3ff737ad5
Thiago Sullivan <thiagosullivan12@gmail.com>
2022-06-21 11:08:14 -0300

    Adding footer

---
+++ components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Image from 'next/image';
+import LogoFooter from '../../assets/ibm-logo.png';
+import { FooterContainer } from './style';
+
+function Footer() {
+  return (
+    <FooterContainer>
+        <Image src={LogoFooter} />
+    </FooterContainer>
+  )
+}
+
+export default Footer
---
+++ components/footer/style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.footer`
+    background-color: ${({theme}) => theme.secondary};
+    display: flex;
+    justify-content: center;
+    padding: 3rem 1rem;
+`;
